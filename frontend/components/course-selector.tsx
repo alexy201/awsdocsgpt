934943b734386227768485e470364ea8a6d15dda
Matt Bertrand <mrbertrand@gmail.com>
2024-08-28 16:53:06 -0400

    Filter reults by course, some frontend work

--- frontend/components/course-selector.tsx
+++ frontend/components/course-selector.tsx
@@ -1,15 +1,26 @@
+import * as React from "react";
+import {useRef} from "react";
 
+export interface CourseSelectorProps
+  extends React.SelectHTMLAttributes<HTMLSelectElement> {}
 
-export default function CourseSelector() {
-  return (
-    <label>
-      Pick a Course:
-      <select name="selectedCourse" defaultValue="All Courses">
-        <option value="">All Courses</option>
-        <option value="Theory of Numbers">Theory of Numbers</option>
-        <option value="Course 2">Course 2</option>
-        <option value="Course 3">Course 3</option>
-      </select>
-    </label>
-  );
-}
+const CourseSelector = React.forwardRef<HTMLSelectElement, CourseSelectorProps>(
+    ({ className, ...props }, ref) => {
+      const selectorRef = useRef<HTMLSelectElement | null>(null)
+      const select = selectorRef.current!
+      return (
+        <label>
+          Pick a Course:
+          <select name="selectedCourse" defaultValue="All Courses" ref={selectorRef} {...props}>
+            <option value="">All Courses</option>
+            <option value="Theory of Numbers">Theory of Numbers</option>
+            <option value="Course 2">Course 2</option>
+            <option value="Course 3">Course 3</option>
+          </select>
+        </label>
+      );
+})
+
+CourseSelector.displayName = "CourseSelector";
+
+export default CourseSelector;
